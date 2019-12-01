1cee9bd04490744ced4a3797d9668a683d173eba
League Student <league@iMac-34.attlocal.net>
2019-12-01 12:09:12 -0800

    Finished mod 1
    :)

---
+++ src/_07_binary_converter/BinaryConverter.java
@@ -0,0 +1,63 @@
+package _07_binary_converter;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class BinaryConverter implements ActionListener{
+	JTextField txtFil = new JTextField(20);
+	JLabel labe = new JLabel();
+	JFrame frame = new JFrame();
+	JPanel pane = new JPanel();
+	JButton butto = new JButton();
+	public void run() {
+	
+		butto.setText("Convert");
+		frame.setVisible(true);
+		pane.add(txtFil);
+		pane.add(butto);
+		labe.setText("Output");
+		pane.add(labe);
+		frame.add(pane);
+		frame.pack();
+		
+		butto.addActionListener(this);
+		
+
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		labe.setText(convert(txtFil.getText()));
+		frame.pack();
+		JOptionPane.showMessageDialog(null, convert(txtFil.getText()));
+	}
+	
+	String convert(String input) {
+        if(input.length() != 8){
+             JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+             return "-";
+        }
+        String binary = "[0-1]+";    //must contain numbers in the given range
+        if (!input.matches(binary)) {
+             JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+             return "-";
+        }
+        try {
+             int asciiValue = Integer.parseInt(input, 2);
+             
+             return String.valueOf(asciiValue);
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+             return "-";
+        }
+   }
+}
+
+
---
+++ src/_07_binary_converter/BinaryConverterRunner.java
@@ -0,0 +1,10 @@
+package _07_binary_converter;
+
+public class BinaryConverterRunner {
+
+	public static void main(String[] args) {
+		new BinaryConverter().run();
+
+	}
+
+}
